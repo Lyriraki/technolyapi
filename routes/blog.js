3d9e18e4cea0b88e5dc627cc7bcfe7772e596694
Lyriraki <Luckyarif21@gmail.com>
2021-01-21 15:39:07 +0700

    Add crud blog API with show photo BLOG

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { requireSignin, adminMiddleware } = require("../controllers/auth");
-const { create } = require("../controllers/blog");
+const {
+  create,
+  list,
+  listAllBlogsCategoriesTags,
+  read,
+  remove,
+  update,
+  photo,
+} = require("../controllers/blog");
 
 router.post("/blog", requireSignin, adminMiddleware, create);
+router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
+
+router.get("/blogs", list);
+router.get("/blog/:slug", read);
+router.get("/blog/photo/:slug", photo);
+
+router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
+
+router.put("/blog/:slug", requireSignin, adminMiddleware, update);
 
 module.exports = router;
